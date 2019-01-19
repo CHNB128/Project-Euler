ec47e34a1a706724eae576df2d3c684da2173cfd
drake.aren <drake.aren@protonmail.com>
2019-01-19 21:16:22 +0600

    solve 1 problem

---
+++ project_euler/lib/problem_1.ex
@@ -0,0 +1,15 @@
+defmodule Problem1 do
+  # Problem 1
+  # Multiples of 3 and 5
+
+  # Description:
+  # If we list all the natural numbers below 10 that are multiples of 3 or 5,
+  # we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+  # TODO:
+  # Find the sum of all the multiples of 3 or 5 below 1000.
+
+  def solve() do
+    Enum.sum(for i <- 1..1000, rem(i, 3) == 0 or rem(i, 5) == 0, do: i)
+  end
+end
