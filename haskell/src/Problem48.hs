caec9bf3572e5d0ace9dc4f001386030cd6581eb
drake.aren <drake.aren@protonmail.com>
2019-02-17 19:54:48 +0600

    solve problem 48 by haskell

---
+++ haskell/src/Problem48.hs
@@ -0,0 +1,16 @@
+module Problem48
+( solving )
+where
+
+{-
+
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
+-}
+
+selfPowerFrom :: Int -> [Int]
+selfPowerFrom n = n ^ n : selfPowerFrom (n + 1)
+
+solving = reverse $ take 10 $ reverse $ show $ sum $ take 1000 $ selfPowerFrom 1
