6d3d80bdbda9441dd392d914ecbcf48e2ccb8363
drake.aren <drake.aren@protonmail.com>
2019-02-23 12:09:12 +0600

    fix problem48 data types

--- haskell/src/Problem48.hs
+++ haskell/src/Problem48.hs
@@ -10,7 +10,7 @@ Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
 -}
 
-selfPowerFrom :: Int -> [Int]
+selfPowerFrom :: Integer -> [Integer]
 selfPowerFrom n = n ^ n : selfPowerFrom (n + 1)
 
 solving = reverse $ take 10 $ reverse $ show $ sum $ take 1000 $ selfPowerFrom 1
